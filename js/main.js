f6d6d49443a5b9644e81ec71b897e7081aa52f11
ankita8800 <119149080+ankita8800@users.noreply.github.com>
2023-12-12 15:30:54 +0530

    Add files via upload
    hello, this is custom website

---
+++ js/main.js
@@ -0,0 +1,44 @@
+
+  $(document).ready(function() {
+  $('.demo-5').owlCarousel({
+    loop:true,
+    dots: false,
+    nav:true,
+    margin:20,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:2
+      },
+      1000:{
+        items:4
+         }
+      }
+    });
+     
+     
+  })
+
+  $(document).ready(function() {
+  $('.testimonial-sec').owlCarousel({
+    loop:true,
+    dots: false,
+    nav:true,
+    margin:20,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:1
+      },
+      1000:{
+        items:2
+         }
+      }
+    });
+     
+     
+  })
